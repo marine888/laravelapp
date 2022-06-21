6137c484cf38f41458cc2c313dc6ad64c7a7bfd2
marine888 <mrnkj.819@gmail.com>
2022-06-21 14:55:24 +0900

    テキスト P96まで

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -7,11 +7,11 @@
    インデックスページ
 @endsection
 
+
+
 @section('content')
    <p>ここが本文のコンテンツです。</p>
-   <p>必要なだけ記述できます。</p>
-@endsection
-
-@section('footer')
-copyright 2020 tuyano.
+   <ul>
+   @each('components.item', $data, 'item')
+   </ul>
 @endsection
