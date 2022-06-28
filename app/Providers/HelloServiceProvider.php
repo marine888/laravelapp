fde84c11214955124d58d1554df1d341c692e7dd
marine888 <mrnkj.819@gmail.com>
2022-06-28 14:45:17 +0900

    テキスト P160まで

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -3,16 +3,18 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Validator;
+use App\Http\Validators\HelloValidator;
 
 class HelloServiceProvider extends ServiceProvider
 {
-   public function boot()
-   {
-       View::composer(
-           'hello.index', function($view){
-               $view->with('view_message', 'composer message!');
-           }
-       );
-   }
 
+
+    public function boot()
+    {
+       Validator::extend('hello', function($attribute, $value,
+               $parameters, $validator) {
+           return $value % 2 == 0;
+       });
+    }
 }
