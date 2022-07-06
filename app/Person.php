ed91fd00e873906caaaa804e727018b9cfba655b
marine888 <mrnkj.819@gmail.com>
2022-07-06 12:24:28 +0900

    テキスト P253 グローバルスコープのコメントアウト

--- app/Person.php
+++ app/Person.php
@@ -23,14 +23,14 @@ public function scopeAgeLessThan($query, $n)
    return $query->where('age', '<=', $n);
 }
 
-protected static function boot()
-{
-   parent::boot();
+// protected static function boot()
+// {
+//    parent::boot();
 
-   static::addGlobalScope('age', function (Builder $builder) {
-       $builder->where('age', '>', 20);
-   });
-}
+//    static::addGlobalScope('age', function (Builder $builder) {
+//        $builder->where('age', '>', 20);
+//    });
+// }
 
 }
 
