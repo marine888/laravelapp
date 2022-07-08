076efae9170dc5c000e2d566ac66566c5784aeff
marine888 <mrnkj.819@gmail.com>
2022-07-08 11:41:12 +0900

    P285 関連レコードの有無

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -9,13 +9,11 @@ class PersonController extends Controller
 {
    public function index(Request $request)
    {
-       $items = Person::all();
-       return view('person.index', ['items' => $items]);
+      $hasItems = Person::has('boards')->get();
+      $noItems = Person::doesntHave('boards')->get();
+      $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
+      return view('person.index', $param);
    }
-   public function find(Request $request)
-{
-   return view('person.find',['input' => '']);
-}
 
 public function search(Request $request)
 {
