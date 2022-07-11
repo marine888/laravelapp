680350dd1207ee82be4e25422736122c27cd87e6
marine888 <mrnkj.819@gmail.com>
2022-07-11 11:35:38 +0900

    テキスト P309

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -86,5 +86,25 @@ public function show(Request $request)
        ->get();
    return view('hello.show', ['items' => $items]);
 }
+
+// testアクションメソッドを追記
+public function rest(Request $request)
+{
+   return view('hello.rest');
+}
+
+// アクション作成
+public function ses_get(Request $request)
+{
+   $sesdata = $request->session()->get('msg');
+   return view('hello.session', ['session_data' => $sesdata]);
+}
+
+public function ses_put(Request $request)
+{
+   $msg = $request->input;
+   $request->session()->put('msg', $msg);
+   return redirect('hello/session');
+}
 }
 
